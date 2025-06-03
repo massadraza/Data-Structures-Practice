3011fa8b2c56a11c5f5beefdadaab7a9d2061908
massadraza <massad.raza@hotmail.com>
2025-06-02 22:11:45 -0400

    Applying Stacks to balancing brackets

--- Stacks/brackets.py
+++ Stacks/brackets.py
@@ -1,7 +1,9 @@
 # Massad Raza
 # Using the data structures stacks to determine if brackets are balanced
 
-def yo():
-    print("Hello")
+def balanced(p_string):
+    s = Stack()        # declare stack object
+    isBalanced = true  # assume that it is balanced
+    index = 0          # start at index 0
 
-yo()
+  #  while index < len(p_string) and isBalanced:
