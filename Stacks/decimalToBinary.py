812dc6f50cd6a255b7f15d41dc2d73ca18c4df4c
massadraza <massad.raza@hotmail.com>
2025-06-06 22:47:35 -0400

    Practicing Stacks

---
+++ Stacks/decimalToBinary.py
@@ -0,0 +1,25 @@
+from stack import Stack
+
+def convert_int_to_bin(dec_num):
+
+    if dec_num == 0:
+        return 0
+
+    a = Stack()
+
+    while dec_num != 0:
+        bin = dec_num % 2
+        a.push(bin)
+        dec_num = int(dec_num / 2)
+
+    finalBin = ""
+
+    while not a.is_empty():
+        finalBin = finalBin + str(a.pop())
+
+    return finalBin
+
+print(convert_int_to_bin(37))
+print(convert_int_to_bin(53))
+print(convert_int_to_bin(36))
+print(convert_int_to_bin(37))
